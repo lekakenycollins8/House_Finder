e000879fd7a8bc01a1aba75dc5df7a17074abda5
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-09-11 18:06:49 +0300

    updated User model with role then created route for role assignment

--- models/User.js
+++ models/User.js
@@ -20,6 +20,10 @@ const User = sequelize.define('User', {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    role: {
+        type: DataTypes.ENUM('landlord', 'renter'),
+        allowNull: true,
+    }
 });
 
 module.exports = User;
